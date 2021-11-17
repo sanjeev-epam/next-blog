dcdead96b63a4eb4283aa08f21dbdf21cd2984e1
Sanjeev Kumar Gadamsetti <sanjeev_gadamsetti@epam.com>
2021-11-17 18:02:26 -0500

    Updated c

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <p>>Level 2</p>
+        <p>Level 2</p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
